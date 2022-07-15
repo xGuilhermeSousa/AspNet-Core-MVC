330becb8631a86cbc4bea7ae98172a1c1fedc181
xGuiSousa <guialvesdesouza@hotmail.com>
2022-07-15 11:18:44 -0300

    Primeiro MVC - Department

--- SalesWebMVC/Controllers/HomeController.cs
+++ SalesWebMVC/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using SalesWebMVC.Models.ViewModels;
 
 namespace SalesWebMVC.Controllers
 {
